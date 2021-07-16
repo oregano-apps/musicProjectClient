5380f22f8db0f4a4a304a7086353d2e2143ec18d
Meir Shaish <almogmaimon674@gmail.com>
2021-07-16 19:21:24 +0300

    add server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-print('server here')
+const express = require('express')
+const PORT = 5000
+
+const app = express()
+
+app.listen(PORT, () => {
+    console.log('the server is app and running')
+})
